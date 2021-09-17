20abd3916ea09b7a9b483002f7b3a1e209eb14e8
foggynight <foggynight709@gmail.com>
2021-09-17 19:45:40 -0230

    Replace {read,write}-tape with head-{read,write}
    Also moved head->index and head-{read,write} to the head module.

--- src/head.scm
+++ src/head.scm
@@ -1,7 +1,21 @@
-(declare (unit head))
+(declare (unit head)
+         (uses tape))
 
 (define (move-left head)
   (- head 1))
 
 (define (move-right head)
   (+ head 1))
+
+(define (head->index head)
+  (if (negative? head)
+      (- (* (abs head) 2) 1)
+      (* head 2)))
+
+;; Read a character from a tape at the location of a head.
+(define (head-read head tape)
+  (tape-ref tape (head->index head)))
+
+;; Write a character to a tape at the location of a head.
+(define (head-write head tape)
+  (tape-ref tape (head->index head)))
