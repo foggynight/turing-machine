88a3a6674a7f6bef013bef4adac1f7b2c5b82792
Robert Coffey <foggynight709@gmail.com>
2021-10-05 15:27:40 -0230

    Add TAPE->STRING and update DISPLAY-TAPE

--- src/cli.scm
+++ src/cli.scm
@@ -10,18 +10,27 @@
 
 (import (chicken format)
         (chicken io)
+        (chicken port)
         (chicken process-context))
+
+;; Convert TAPE into its string representation, omitting any leading and
+;; trailing blank cells.
+;; (tape->string tape) => string
+(define (tape->string tape)
+  (with-output-to-string
+    (lambda ()
+      (let ((first-char (tape-first-char tape))
+            (last-char (tape-last-char tape)))
+        (unless (null? first-char)
+          (let loop ((head (make-head first-char)))
+            (when (<= head last-char)
+              (display (tape-read tape head))
+              (loop (move-head head 'right)))))))))
 
 ;; Display TAPE, omitting any leading and trailing blank cells.
 ;; (display-tape tape) => unspecified
 (define (display-tape tape)
-  (let ((first-char (tape-first-char tape))
-        (last-char (tape-last-char tape)))
-    (unless (null? first-char)
-      (let loop ((head (make-head first-char)))
-        (when (<= head last-char)
-          (display (tape-read tape head))
-          (loop (move-head head 'right)))))))
+  (display (tape->string tape)))
 
 (define (main path)
   (define program-string (with-input-from-file path
