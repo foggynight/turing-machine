d51a70146f6762796c383c58ead31a37eab215d8
Robert Coffey <foggynight709@gmail.com>
2021-10-05 16:15:39 -0230

    Add HALT-STATE?

--- src/state.scm
+++ src/state.scm
@@ -9,3 +9,10 @@
 ;; (state=? state state) => boolean
 (define (state=? state0 state1)
   (string=? state0 state1))
+
+;; Determine if STATE is a halt state, that is, accept, reject, or error.
+;; (halt-state? state) => boolean
+(define (halt-state? state)
+  (or (state=? state accept-state)
+      (state=? state reject-state)
+      (state=? state error-state)))
