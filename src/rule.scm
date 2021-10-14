a28f466e1489c6aca0a6447fcfae01b44f1841b8
Robert Coffey <foggynight709@gmail.com>
2021-10-14 00:08:00 -0230

    Update rule accessor functions to use rule-ref

--- src/rule.scm
+++ src/rule.scm
@@ -16,8 +16,8 @@
   (list-copy rule))
 
 ;; Transition rule accessor functions.
-(define (rule-current-state rule) (list-ref rule 0))
-(define (rule-read-symbol rule) (list-ref rule 1))
-(define (rule-next-state rule) (list-ref rule 2))
-(define (rule-write-symbol rule) (list-ref rule 3))
-(define (rule-move-direction rule) (list-ref rule 4))
+(define (rule-current-state rule) (rule-ref rule 0))
+(define (rule-read-symbol rule) (rule-ref rule 1))
+(define (rule-next-state rule) (rule-ref rule 2))
+(define (rule-write-symbol rule) (rule-ref rule 3))
+(define (rule-move-direction rule) (rule-ref rule 4))
