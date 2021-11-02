1b009d61ba94e8ebc512c6ea3516b64fdc8bde01
Robert Coffey <foggynight709@gmail.com>
2021-11-02 01:28:55 -0230

    Remove unused list utility functions

--- src/utils.scm
+++ src/utils.scm
@@ -2,25 +2,6 @@
 
 (declare (unit utils))
 
-;;; list -----------------------------------------------------------------------
-
-;; Get a sublist obtained by omitting all but the first K elements of LST. It is
-;; an error for LST to contain less than K elements.
-;; (list-head list integer) -> list
-(define (list-head lst k)
-  (if (positive? k)
-      (cons (car lst) (list-head (cdr lst) (- k 1)))
-      '()))
-
-;; Get a list containing the elements from the range [START, END) of LST. START
-;; and END must be non-negative integers. It is an error for START to be greater
-;; than L-1 or for END to be greater than L, where L is the length of LST.
-;; (sublist list integer integer) -> list
-(define (sublist lst start end)
-  (list-head (list-tail lst start) (- end start)))
-
-;;; string ---------------------------------------------------------------------
-
 ;; Get the first character of STR, or false if STR is empty.
 ;; (first-character string) -> character | false
 (define (first-character str)
