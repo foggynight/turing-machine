6092374f614304a8023a755705ab48488e5f26fa
Robert Coffey <foggynight709@gmail.com>
2021-11-01 23:26:18 -0230

    Add FIRST-CHARACTER and update...
    ...FIRST-NON-WHITESPACE function comment.

--- src/utils.scm
+++ src/utils.scm
@@ -21,7 +21,14 @@
 
 ;;; string ---------------------------------------------------------------------
 
-;; Return the first non-whitespace character of STR, or false if STR does not
+;; Get the first character of STR, or false if STR is empty.
+;; (first-character string) -> character | false
+(define (first-character str)
+  (if (= (string-length str) 0)
+      #f
+      (string-ref str 0)))
+
+;; Get the first non-whitespace character of STR, or false if STR does not
 ;; contain any non-whitespace characters.
 ;; (first-non-whitespace string) -> character | false
 (define (first-non-whitespace str)
