2bca9af28f790d036fcc3a6a5dc515f1caf29b30
Robert Coffey <foggynight709@gmail.com>
2021-11-02 00:42:48 -0230

    Update parser to detect, configure for, and...
    ...parse a multi-tape program.

--- src/global.scm
+++ src/global.scm
@@ -1,17 +1,27 @@
 ;;;; global.scm - Turing machine global variables.
 
+;; TODO Add CONF directives.
+
 (declare (unit global))
 
-(define blank-character #\_)
-(define comment-character #\;)
-(define wildcard-character #\*)
+;;; dynamic - Can be modified by CONF directives. ------------------------------
 
-(define initial-state "0")
-(define accept-state "A")
-(define reject-state "R")
-(define error-state "E")
-(define extra-halt-states '())
+(define initial-state "0")              ; STATE:INITIAL
+(define accept-state "A")               ; STATE:ACCEPT
+(define reject-state "R")               ; STATE:REJECT
+(define error-state "E")                ; STATE:ERROR
+(define extra-halt-states '())          ; STATE:EXTRA-HALTS
 
-(define left-character #\L)
-(define right-character #\R)
-(define stay-character #\S)
+(define comment-character #\;)          ; CHAR:COMMENT
+(define blank-character #\_)            ; CHAR:BLANK
+(define wildcard-character #\*)         ; CHAR:WILDCARD
+
+(define left-character #\L)             ; CHAR:LEFT
+(define right-character #\R)            ; CHAR:RIGHT
+(define stay-character #\S)             ; CHAR:STAY
+
+(define tape-count #f)                  ; TAPE:COUNT
+
+;;; static - Can't be modified by CONF directives. -----------------------------
+
+(define move-characters (list left-character right-character stay-character))
--- src/parser.scm
+++ src/parser.scm
@@ -1,4 +1,6 @@
-;;;; parser.scm - Turing machine program parser functions.
+;;;; parser.scm - Turing machine program parser.
+
+;; TODO Include line number in error messages.
 
 (declare (unit parser)
          (uses global)
@@ -6,88 +8,92 @@
 
 (import (chicken string))
 
-;; Remove comments from LINES, where a comment is a line whose first
-;; non-whitespace character is a comment character.
-;; (remove-comments list) -> list
-(define (remove-comments lines)
-  (if (null? lines)
-      '()
-      (let ((line (car lines)))
-        (if (char=? (first-non-whitespace line) comment-character)
-            (remove-comments (cdr lines))
-            (cons line (remove-comments (cdr lines)))))))
-
 ;; Parse a CONF directive and configure the Turing machine.
-;; (parse-conf! string) -> unspecified
+;; (parse-conf! string) -> void
 (define (parse-conf! line)
-  (let ((split-str (string-split line)))
-    (when (< (length split-str) 2)
-      (error "parse-conf!: Invalid CONF directive"))
-    (cond ((string-ci=? (car split-str) "STATE:INITIAL")
-           (set! initial-state (cadr split-str)))
-          ((string-ci=? (car split-str) "STATE:ACCEPT")
-           (set! accept-state (cadr split-str)))
-          ((string-ci=? (car split-str) "STATE:REJECT")
-           (set! reject-state (cadr split-str)))
-          ((string-ci=? (car split-str) "STATE:ERROR")
-           (set! error-state (cadr split-str)))
-          ((string-ci=? (car split-str) "STATE:EXTRA-HALTS")
-           (set! extra-halt-states (append extra-halt-states (cdr split-str))))
-          (else (error "parse-conf!: Invalid CONF directive")))))
+  (define split (string-split line))
+  (when (< (length split) 2)
+    (error "parse-conf!: Invalid CONF directive"))
+  (cond ((string-ci=? (car split) "STATE:INITIAL")
+         (set! initial-state (cadr split)))
+        ((string-ci=? (car split) "STATE:ACCEPT")
+         (set! accept-state (cadr split)))
+        ((string-ci=? (car split) "STATE:REJECT")
+         (set! reject-state (cadr split)))
+        ((string-ci=? (car split) "STATE:ERROR")
+         (set! error-state (cadr split)))
+        ((string-ci=? (car split) "STATE:EXTRA-HALTS")
+         (set! extra-halt-states (append extra-halt-states (cdr split))))
+        ((string-ci=? (car split) "TAPE:COUNT")
+         (set! tape-count (string->number (cadr split))))
+        (else (error "parse-conf!: Invalid CONF directive"))))
 
-;; Parse and remove the CONF section of LINES, should it contain one, and return
-;; LINES. Each line in the CONF section is passed to PARSE-CONF! to configure
-;; the Turing machine.
-;; (parse-and-remove-conf! list) -> list
-(define (parse-and-remove-conf! lines)
-  (define (find-line test)
-    (let loop ((i 0)
-               (lines lines))
-      (if (null? lines)
-          -1
-          (if (test (car lines))
-              i
-              (loop (+ i 1) (cdr lines))))))
-  (define (conf-begin? line)
-    (let ((split-str (string-split line)))
-      (string-ci=? line "CONF:BEGIN")))
-  (define (conf-end? line)
-    (let ((split-str (string-split line)))
-      (string-ci=? line "CONF:END")))
-  (define (remove-conf start end)
-    (define (aux lines i)
-      (if (null? lines)
-          '()
-          (if (and (>= i start)
-                   (<= i end))
-              (aux (cdr lines) (+ i 1))
-              (cons (car lines) (aux (cdr lines) (+ i 1))))))
-    (aux lines 0))
-  (define conf-start (find-line conf-begin?))
-  (define conf-end (find-line conf-end?))
-  (unless (= conf-start conf-end -1)
-    (when (or (= conf-start -1)
-              (= conf-end -1)
-              (>= conf-start conf-end))
-      (error "parse-and-remove-conf!: Invalid CONF section")))
-  (for-each parse-conf! (sublist lines (+ conf-start 1) conf-end))
-  (remove-conf conf-start conf-end))
+;; Get the number of tapes used in the rule represented by CURR and NEXT.
+;; (get-tape-count list list) -> integer >= 0
+(define (get-tape-count curr next)
+  (define (get-read-count)
+    (- (length curr) 1))
+  (define (get-write-count)
+    (inexact->exact (truncate (/ (- (length next) 1) 2))))
+  (let ((count (get-read-count)))
+    (unless (= count (get-write-count))
+      (error "get-tape-count: Invalid rule: Read/write counts not equal"))
+    count))
 
-;; Parse a transition rule from STR.
-;; (parse-rule string) -> rule
-;; e.g. (parse-rule "0 0 -> 0 0 R") -> ("0" #\0 "0" #\0 #\R)
-(define (parse-rule str)
-  (let ((split-str (string-split str)))
-    (list (list-ref split-str 0)
-          (string-ref (list-ref split-str 1) 0)
-          (list-ref split-str 3)
-          (string-ref (list-ref split-str 4) 0)
-          (string-ref (list-ref split-str 5) 0))))
+;; Parse and return the transition rule contained within CURR and NEXT, check if
+;; the rule uses the right number of tapes, check if the move characters used in
+;; the rule are valid, and set the value of TAPE-COUNT if necessary.
+;; (parse-rule! list list) -> rule
+(define (parse-rule! curr next)
+  (define (validate-read/write-string str)
+    (= (string-length str) 1))
+  (if tape-count
+      (unless (= (get-tape-count curr next) tape-count)
+        (error "parse-rule!: Invalid rule: Invalid tape count"))
+      (set! tape-count (get-tape-count curr next)))
+  (unless (not (memq #f (map (lambda (e)
+                               (member (string-ref e 0) move-characters))
+                             (list-tail next (+ tape-count 1)))))
+    (error "parse-rule!: Invalid rule: Invalid move character"))
+  (list (cons (car curr) (map first-character (cdr curr)))
+        (cons (car next) (map first-character (cdr next)))))
 
-;; Parse a program from STR, where a program is a list of transition rules. Also
-;; configures the Turing machine using the CONF section of STR, should it
-;; contain one.
+;; Parse and return the program contained within STR, where a program is a list
+;; of transition rules. This function also configures the Turing machine if the
+;; program contains a CONF section.
 ;; (parse-program! string) -> list
 (define (parse-program! str)
-  (let ((lines (string-split str (string #\newline))))
-    (map parse-rule (parse-and-remove-conf! (remove-comments lines)))))
+  (define (line-comment? line)
+    (char=? (first-non-whitespace line) comment-character))
+  (define (line-conf-begin? line)
+    (string-ci=? (car (string-split line)) "CONF:BEGIN"))
+  (define (line-conf-end? line)
+    (string-ci=? (car (string-split line)) "CONF:END"))
+  (define in-conf #f)
+  (define rules '())
+  (let loop ((lines (string-split str (string #\newline)))
+             (line-number 1))
+    (unless (null? lines)
+      (let ((line (car lines)))
+        (cond ((line-comment? line))
+              ((line-conf-begin? line)
+               (if in-conf
+                   (error "parse-program!: Invalid CONF section")
+                   (set! in-conf #t)))
+              ((line-conf-end? line)
+               (if in-conf
+                   (set! in-conf #f)
+                   (error "parse-program!: Invalid CONF section")))
+              (in-conf (parse-conf! line))
+              (else
+               (let ((split (string-split line)))
+                 (unless (member "->" split)
+                   (error "parse-program!: Invalid rule: Separator not found"))
+                 (let ((curr (let loop ((lst split))
+                               (if (string=? (car lst) "->")
+                                   '()
+                                   (cons (car lst) (loop (cdr lst))))))
+                       (next (cdr (member "->" split))))
+                   (set! rules (cons (parse-rule! curr next) rules)))))))
+      (loop (cdr lines) (+ line-number 1))))
+  (reverse rules))
