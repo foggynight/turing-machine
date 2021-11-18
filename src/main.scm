5107d064cca59980490d2f749b9dbdd97f0bb79f
Robert Coffey <foggynight709@gmail.com>
2021-11-18 01:34:55 -0330

    Add eval and repl modes

--- src/main.scm
+++ src/main.scm
@@ -21,7 +21,33 @@
   (display program-string)
   (format #t ";; ~A~%" (make-string 77 #\-)))
 
-(define (main path)
+;; Evaluate a single input.
+;; (tm-eval) -> void
+(define (tm-eval)
+  (define line (read-line))
+  (format #t "~A " line)
+  (engine-reset! line)
+  (engine-skip!)
+  (format #t "-> ~A, ~A~%"
+          (engine-state)
+          (tape->string (car (engine-tapes)))))
+
+;; Evaluate inputs in a REPL.
+;; (tm-repl) -> void
+(define (tm-repl #!optional (i 0))
+  (format #t "~A> " i)
+  (let ((line (read-line)))
+    (when (eof-object? line)
+      (newline)
+      (exit))
+    (engine-reset! line)
+    (engine-skip!)
+    (format #t "-> ~A, ~A~%"
+            (engine-state)
+            (tape->string (car (engine-tapes))))
+    (tm-repl (+ i 1))))
+
+(define (main mode path)
   (define program-string
     (with-input-from-file path
       (lambda ()
@@ -31,17 +57,13 @@
                     (read-lines))))))
   (display-program path program-string)
   (engine-init! program-string)
-  (let repl-loop ((repl-i 0))
-    (format #t "~A> " repl-i)
-    (let ((line (read-line)))
-      (when (eof-object? line)
-        (newline)
-        (exit))
-      (engine-reset! line)
-      (engine-skip!)
-      (format #t "-> ~A, ~A~%"
-              (engine-state)
-              (tape->string (car (engine-tapes))))
-      (repl-loop (+ repl-i 1)))))
+  (if (eq? mode 'eval)
+      (tm-eval)
+      (tm-repl)))
 
-(main (car (command-line-arguments)))
+(let* ((args (command-line-arguments))
+       (mode (string->symbol (car args))))
+  (if (or (eq? mode 'eval)
+          (eq? mode 'repl))
+      (main mode (cadr args))
+      (format #t "Error: Invalid MODE: ~A" mode)))
